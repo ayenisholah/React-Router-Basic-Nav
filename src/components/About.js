bd6d50a48f0bbaeaec2864d8ec93daabfe43a3d6
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-17 12:32:50 +0100

    About refactored

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const About = () => <h1>About</h1>;
+function About() {
+  return (
+    <div>
+      <h1>React Router Basic About</h1>
+      <h4>Nothing to see here</h4>
+    </div>
+  ); 
+}
 
 export default About;
