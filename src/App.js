5e79a581aba394dcb04220fce65d36f331923ac3
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-17 14:13:11 +0100

    Navigation component refactored.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 import AvengerPage from './components/AvengerPage';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import avengers from './data';
 
@@ -15,14 +15,14 @@ class App extends Component {
     };
   }
 
-  render() {
+  render() { 
     return (
-      <div>
-    <Navigation />
-    <Route exact path='/' component={Home} />
-    <Route exact path='/about' component={About} />
-    <Route path='/about/:id' component={AvengerPage} />
-    <Route exact path='/contact' component={Contact} />
+  <div className='App'>
+  <Navigation />
+  <Route exact path='/' component={Home} />
+  <Route exact path='/about' component={About} />
+  <Route path='/about/:id' component={AvengerPage} />
+  <Route exact path='/contact' component={Contact} />
   </div>
     )
   }
--- src/components/AvengerPage.jsx
+++ src/components/AvengerPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import avengers from '../data';
 
 function AvengerPage(props) {
