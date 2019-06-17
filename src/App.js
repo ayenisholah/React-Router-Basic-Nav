1a888aedbe7c143b0d43fa6748bd92714f17003e
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-17 12:57:15 +0100

    App refactored and Route components for navigation links created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
-const App = () => (
-  <div>
+import avengers from './data';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: avengers
+    };
+  }
+
+  render() {
+    return (
+      <div>
     <Navigation />
+    <Route exact path='/' component={Home} />
+    <Route exact path='/about' component={About} />
+    <Route exact path='/contact' component={Contact} />
   </div>
-);
+    )
+  }
+}
 
 export default App;
